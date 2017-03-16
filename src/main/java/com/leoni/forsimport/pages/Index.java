02d9c3b75c2a7152bfbeb10322232a9a63771312
BacheIbtihel <bacheibtihel@gmail.com>
2017-03-16 15:36:55 +0100

    Modified index page

--- src/main/java/com/leoni/forsimport/pages/Index.java
+++ src/main/java/com/leoni/forsimport/pages/Index.java
@@ -1,76 +1,34 @@
 package com.leoni.forsimport.pages;
 
+import java.io.File;
 
-import org.apache.tapestry5.Block;
-import org.apache.tapestry5.EventContext;
-import org.apache.tapestry5.SymbolConstants;
-import org.apache.tapestry5.annotations.InjectPage;
-import org.apache.tapestry5.annotations.Log;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.tapestry5.PersistenceConstants;
+import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
-import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.ioc.annotations.Symbol;
-import org.apache.tapestry5.services.HttpError;
-import org.apache.tapestry5.services.ajax.AjaxResponseRenderer;
-import org.slf4j.Logger;
-
-import java.util.Date;
+import org.apache.tapestry5.upload.services.UploadedFile;
 
 /**
- * Start page of application forsimport.
+ * Upload Excel file
  */
-public class Index
-{
-  @Inject
-  private Logger logger;
+public class Index {
+	@Property
+	private UploadedFile file;
 
-  @Inject
-  private AjaxResponseRenderer ajaxResponseRenderer;
+	public void onSuccess() {
+		File copied = new File("C:/ExcelFile/" + file.getFileName());
 
-  @Property
-  @Inject
-  @Symbol(SymbolConstants.TAPESTRY_VERSION)
-  private String tapestryVersion;
+		file.write(copied);
+	}
 
-  @InjectPage
-  private About about;
+	@Persist(PersistenceConstants.FLASH)
+	@Property
+	private String message;
 
-  @Inject
-  private Block block;
+	Object onUploadException(FileUploadException ex) {
+		message = "Upload exception: " + ex.getMessage();
 
+		return this;
+	}
 
-  // Handle call with an unwanted context
-  Object onActivate(EventContext eventContext)
-  {
-    return eventContext.getCount() > 0 ?
-        new HttpError(404, "Resource not found") :
-        null;
-  }
-
-
-  Object onActionFromLearnMore()
-  {
-    about.setLearn("LearnMore");
-
-    return about;
-  }
-
-  @Log
-  void onComplete()
-  {
-    logger.info("Complete call on Index page");
-  }
-
-  @Log
-  void onAjax()
-  {
-    logger.info("Ajax call on Index page");
-
-    ajaxResponseRenderer.addRender("middlezone", block);
-  }
-
-
-  public Date getCurrentTime()
-  {
-    return new Date();
-  }
 }
