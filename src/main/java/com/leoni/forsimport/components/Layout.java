2ddad910f3e1c05affc78c2a8e946fee94bd804f
BacheIbtihel <bacheibtihel@gmail.com>
2017-03-16 15:34:22 +0100

    Add an import page

--- src/main/java/com/leoni/forsimport/components/Layout.java
+++ src/main/java/com/leoni/forsimport/components/Layout.java
@@ -1,50 +1,44 @@
 package com.leoni.forsimport.components;
 
-import org.apache.tapestry5.*;
-import org.apache.tapestry5.alerts.AlertManager;
-import org.apache.tapestry5.annotations.*;
-import org.apache.tapestry5.corelib.components.Form;
-import org.apache.tapestry5.corelib.components.PasswordField;
-import org.apache.tapestry5.corelib.components.TextField;
-import org.apache.tapestry5.corelib.components.Zone;
-import org.apache.tapestry5.ioc.annotations.*;
 import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.ioc.annotations.Symbol;
 
 /**
  * Layout component for pages of application test-project.
  */
-@Import(module="bootstrap/collapse")
-public class Layout
-{
-  @Inject
-  private ComponentResources resources;
+@Import(module = "bootstrap/collapse")
+public class Layout {
+	@Inject
+	private ComponentResources resources;
 
-  /**
-   * The page title, for the <title> element and the <h1> element.
-   */
-  @Property
-  @Parameter(required = true, defaultPrefix = BindingConstants.LITERAL)
-  private String title;
+	/**
+	 * The page title, for the <title> element and the
+	 * <h1>element.
+	 */
+	@Property
+	@Parameter(required = true, defaultPrefix = BindingConstants.LITERAL)
+	private String title;
 
-  @Property
-  private String pageName;
+	@Property
+	private String pageName;
 
-  @Property
-  @Inject
-  @Symbol(SymbolConstants.APPLICATION_VERSION)
-  private String appVersion;
+	@Property
+	@Inject
+	@Symbol(SymbolConstants.APPLICATION_VERSION)
+	private String appVersion;
 
-  public String getClassForPageName()
-  {
-    return resources.getPageName().equalsIgnoreCase(pageName)
-        ? "active"
-        : null;
-  }
+	public String getClassForPageName() {
+		return resources.getPageName().equalsIgnoreCase(pageName) ? "active" : null;
+	}
 
-  public String[] getPageNames()
-  {
-    return new String[]{"Index", "About", "Contact"};
-  }
+	public String[] getPageNames() {
+		return new String[] { "Index", "About", "Contact", "Import" };
+	}
 
 }
