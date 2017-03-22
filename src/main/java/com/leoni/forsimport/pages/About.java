96801faded3bc38ae6a0d905ad157cdc03aa19cf
BacheIbtihel <bacheibtihel@gmail.com>
2017-03-22 15:40:03 +0100

    check

--- src/main/java/com/leoni/forsimport/pages/About.java
+++ src/main/java/com/leoni/forsimport/pages/About.java
@@ -1,15 +1,8 @@
 package com.leoni.forsimport.pages;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
 
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -17,45 +10,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class About {
 
-	/**
-	 * Méthode de récupération structure de table sous forme de fichier Excel
-	 */
-
-	public static void getExcel(Table table) {
-
-		ArrayList<Column> columns = new ArrayList<Column>();
-		columns = table.getColumns();
-
-		// 1. Créer un Document vide
-		XSSFWorkbook wb = new XSSFWorkbook();
-		// 2. Créer une Feuille de calcul vide
-		Sheet feuille = wb.createSheet("Sheet");
-		feuille.setDefaultColumnWidth(15);
-		// 3. Créer une ligne et mettre qlq chose dedans
-		Row rowName = feuille.createRow((short) 0);
-		Row rowType = feuille.createRow((short) 1);
-		Column column = new Column();
-		for (int i = 0; i < columns.size(); i++) {
-			column = columns.get(i);
-			// 4. COLUMN_NAME
-			Cell colulnName = rowName.createCell(i);
-			colulnName.setCellValue(column.getColumnName());
-			// 5. COLUMN_Type
-			Cell colulnType = rowType.createCell(i);
-			colulnType.setCellValue(column.getColumnType());
-			try {
-				FileOutputStream fileOut = new FileOutputStream(table.getTableName() + ".xlsx");
-				wb.write(fileOut);
-				fileOut.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		System.out.println("Excel created");
-	}
+	
 
 	/**
 	 * read the Excel file
