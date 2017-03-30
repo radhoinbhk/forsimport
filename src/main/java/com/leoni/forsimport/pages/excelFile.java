5021c12d951fbc6927dac8637529096fc62266d8
BacheIbtihel <bacheibtihel@gmail.com>
2017-03-30 15:52:01 +0100

    c

--- src/main/java/com/leoni/forsimport/pages/excelFile.java
+++
@@ -1,8 +0,0 @@
-package com.leoni.forsimport.pages;
-
-import java.util.ArrayList;
-import org.apache.poi.ss.usermodel.Cell;
-
-public class excelFile {
-	ArrayList<Cell> column = new ArrayList<Cell>();
-}
